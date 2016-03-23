d28fbecf41b4f87507ef1a680f07dddd64e0e7bf
Nico Rehwaldt <git_nikku@nixis.de>
2016-03-23 16:32:22 +0100

    chore(project): release v0.14.1

--- dist/bpmn-modeler.js
+++ dist/bpmn-modeler.js
@@ -1,5 +1,5 @@
 /*!
- * bpmn-js - bpmn-modeler v0.14.0
+ * bpmn-js - bpmn-modeler v0.14.1
 
  * Copyright 2014, 2015 camunda Services GmbH and other contributors
  *
@@ -8,7 +8,7 @@
  *
  * Source Code: https://github.com/bpmn-io/bpmn-js
  *
- * Date: 2016-03-21
+ * Date: 2016-03-23
  */
 (function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.BpmnJS = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
 'use strict';
@@ -9002,6 +9002,7 @@ module.exports = {
 
 var find = _dereq_(254),
     any = _dereq_(251),
+    filter = _dereq_(253),
     forEach = _dereq_(255),
     inherits = _dereq_(243);
 
@@ -9091,6 +9092,13 @@ BpmnRules.prototype.init = function() {
     return canAttach([ shape ], target, source, position) || canCreate(shape, target, source, position);
   });
 
+  this.addRule([ 'elements.delete' ], function(context) {
+
+    // do not allow deletion of labels
+    return filter(context.elements, function(e) {
+      return !isLabel(e);
+    });
+  });
 };
 
 BpmnRules.prototype.canConnectMessageFlow = canConnectMessageFlow;
@@ -9652,7 +9660,7 @@ function canInsert(shape, flow, position) {
     canDrop(shape, flow.parent, position));
 }
 
-},{"183":183,"243":243,"251":251,"254":254,"255":255,"45":45,"61":61,"68":68,"70":70}],59:[function(_dereq_,module,exports){
+},{"183":183,"243":243,"251":251,"253":253,"254":254,"255":255,"45":45,"61":61,"68":68,"70":70}],59:[function(_dereq_,module,exports){
 module.exports = {
   __depends__: [
     _dereq_(185)
